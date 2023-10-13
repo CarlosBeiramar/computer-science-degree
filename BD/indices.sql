7da9503e0a28a39dcfcee319fc5ef2ba13e5f568
CarlosBeiramar <carlosbeiramar@gmail.com>
2023-10-13 22:52:07 +0100

    remove BD folder

--- BD/indices.sql
+++
@@ -1,8 +0,0 @@
--- Indicação da Base de Dados de Trabalho.
-USE uberblood;
-
--- Criacao de um índice (INDEX) sobre o atributo "estado" e "data" da tabela "Doacao".
-CREATE INDEX idxEstadoDataDoacao
-	ON Doacao (estado,data);
-
-SHOW INDEX FROM Doacao;
